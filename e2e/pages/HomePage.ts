218758dd2473ed8d2661ffaa90ca0820519dafd9
Pandu Wibisono <subroto.panduwibisono@gmail.com>
2025-12-08 13:55:26 +0700

    Refactor HomePage navigation for improved element selection and add tests for various menu interactions

--- e2e/pages/HomePage.ts
+++ e2e/pages/HomePage.ts
@@ -4,8 +4,14 @@ export class HomePage {
     constructor(private page: Page) { }
 
     async goToMenu(subMenu: string) {
-        await this.page.getByRole('heading', { name: 'Elements' }).click();
-        await this.page.getByRole('listitem').getByText(subMenu, { exact: true }).click();
+        const el = this.page.getByRole('heading', { name: 'Elements' });
+        await el.scrollIntoViewIfNeeded();
+        await el.click();
+
+        const accordion = this.page.locator('[class="accordion"]').getByText(subMenu, { exact: true })
+        await accordion.scrollIntoViewIfNeeded()
+        await accordion.click()
+
         await expect(this.page.locator('h1', { hasText: subMenu })).toBeVisible();
     }
 }
--- e2e/tests/elements.spec.ts
+++ e2e/tests/elements.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { HomePage } from '../pages/HomePage';
 
 const menus = [
@@ -16,9 +16,87 @@ const menus = [
 menus.forEach(({ subMenu }) => {
   test(`access to menu and submenu ${subMenu}`, async ({ page }) => {
     await page.goto('https://demoqa.com');
-
     const home = new HomePage(page);
-
-    await home.goToMenu(subMenu);   // hanya 1 fungsi untuk semua menu
+    await home.goToMenu(subMenu);
   });
+});
+
+test(`user input in Text Box`, async ({ page }) => {
+  await page.goto('https://demoqa.com');
+  const home = new HomePage(page);
+  await home.goToMenu('Text Box');
+
+  await page.locator('[id="userName"]').fill('John Doe');
+  await page.locator('[id="userEmail"]').fill('testingpandu@gmail.com');
+  await page.locator('[id="currentAddress"]').fill('123 Main St, Anytown, USA');
+  await page.locator('[id="permanentAddress"]').fill('456 Elm St, Othertown, USA');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await expect(page.locator('[class="border col-md-12 col-sm-12"]')).toBeVisible();
+});
+
+test(`user check in the box`, async ({ page }) => {
+  await page.goto('https://demoqa.com');
+  const home = new HomePage(page);
+  await home.goToMenu('Check Box');
+
+  await page.locator('[title="Expand all"]').click();
+  await page.getByText('Commands').click();
+  await page.getByText('Veu').click();
+  await page.getByText('General').click();
+  await page.getByText('Excel File.doc').click();
+
+  const items = [
+    'Desktop',
+    'Documents',
+    'Office',
+    'Downloads',
+  ];
+
+  for (const item of items) {
+    await expect(
+      page.locator('label', { hasText: item }).locator('[class="rct-icon rct-icon-half-check"]')
+    ).toBeVisible();
+  }
+  await expect(page.locator('[id="result"]')).toBeVisible();
+  await expect(page.locator('#result')).toHaveText([
+    'You have selected :commandsveugeneralexcelFile',
+  ]);
+});
+
+test(`user input radio button`, async ({ page }) => {
+  await page.goto('https://demoqa.com');
+  const home = new HomePage(page);
+  await home.goToMenu('Radio Button');
+
+  await page.locator('[id="impressiveRadio"]').click({ force: true });
+  await expect(page.getByText('You have selected Impressive')).toBeVisible();
+});
+
+test(`user doing CRUD web tables`, async ({ page }) => {
+  await page.goto('https://demoqa.com');
+  const home = new HomePage(page);
+  await home.goToMenu('Web Tables');
+
+  await page.locator('[id="addNewRecordButton"]').click({ force: true });
+  await page.locator('[id="firstName"]').fill('Pandu');
+  await page.locator('[id="lastName"]').fill('Wibisono');
+  await page.locator('[id="userEmail"]').fill('testingpandu@gmail.com');
+  await page.locator('[id="age"]').fill('30');
+  await page.locator('[id="salary"]').fill('50000');
+  await page.locator('[id="department"]').fill('Engineering');
+  await page.getByRole('button', { name: 'Submit' }).click();
+
+  await expect(page.locator('[role="rowgroup"]').filter({ hasText: 'PanduWibisono30testingpandu@' })).toBeVisible();
+
+  const row = page.locator('[role="rowgroup"]', {
+    hasText: 'Pandu'
+  });
+  await row.locator('span[title="Edit"]').click();
+  await page.locator('[id="salary"]').fill('1000000');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await expect(page.locator('[role="rowgroup"]').filter({ hasText: '1000000' })).toBeVisible();
+
+  await row.locator('span[title="Delete"]').click();
+  await expect(page.locator('[role="rowgroup"]').filter({ hasText: 'Pandu' })).not.toBeVisible();
+
 });
